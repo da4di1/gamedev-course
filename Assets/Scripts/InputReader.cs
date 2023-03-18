bb6c36bbcd8957ffa220d735a5b0206b94b1b02e
da4di1 <antolmachev777@gmail.com>
2023-03-18 20:51:11 +0200

    feat: character horizontal movement and jumping

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerHandler))]
+public class InputReader : MonoBehaviour
+{
+    private PlayerHandler _playerEntity;
+
+    private float _direction;
+
+    private void Start()
+    {
+        _playerEntity = GetComponent<PlayerHandler>();
+    }
+
+    private void Update()
+    {
+        _direction = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _playerEntity.Jump();
+        }
+    }
+
+    private void FixedUpdate() 
+    {
+        _playerEntity.MoveHorizontally(_direction);
+    }
+}
